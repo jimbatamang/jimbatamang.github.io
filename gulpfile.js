b2ceb6e6f7cf3d95f6d413ae10068f8cbface311
Jimba Tamang <jt@ggg.com.au>
2016-03-16 14:36:06 +1100

    added bower, re-organize files.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,14 @@
-var isEnv = process.env.NODE_ENV === 'production'; // prod / dev
+/*
+ * Author: Jimba Tamang (ggg.com.au)
+ * Version: 0.0.2
+ * Create on March 06, 2016
+ * ------------------------------------------------------------------------
+ * ************* Gulp *************
+ * ======================================================================== */
+
+// usage: [ENV_STR = "production/development"] gulp
+
+var env       = process.env.NODE_ENV || 'development';
 
 // Load plugins
 var gulp          = require('gulp'),
@@ -8,85 +18,89 @@ var gulp          = require('gulp'),
     jshint        = require('gulp-jshint'),
     concat        = require('gulp-concat'),
     uglify        = require('gulp-uglify'),
-    imagemin      = require('gulp-imagemin'),
     rename        = require('gulp-rename'),
-    cache         = require('gulp-cache'),
-    cmq           = require('gulp-combine-media-queries'),
     plumber       = require('gulp-plumber'),
-    sourcemaps    = require('gulp-sourcemaps'),
+    clean         = require('gulp-clean'),
+    packageInfo   = require('./package.json'),
     ifElse        = require('gulp-if-else');
 
 
-// Default Tasks
-var defaultTasks = [  'sass',
-                      'css',
-                      'scripts',
-                      'watch'
-                    ]
+// Define assets
+var paths = {
 
-var paths       = {
-                    src: {
-                      sass:    'assets/src/sass/',
-                      scss:    'assets/src/sass/**/*.scss',
-                      css:     ['assets/src/css/flexslider.css',
-                                'assets/src/css/style.css'],
-                      js:      ['assets/src/js/vendor/modernizr.js',
-                                'assets/src/js/vendor/jquery-2.2.1.min.js',
-                                'assets/src/js/vendor/jquery-migrate-1.2.1.min.js',
-                                'assets/src/js/vendor/jquery.flexslider-min.js',
-                                'assets/src/js/scripts.js']
-                    },
-                    dest: {
-                      sass:    'assets/src/css',
-                      css:     'assets/build', 
-                      js:      'assets/build'
-                    }
-                  };
+      src: {
+        sass:       'assets/src/sass/style.scss',
+
+        css:        ['assets/src/css/*.css',
+                     'assets/_temp/css/*.css',
+                    ],
+
+        custom_js:  ['assets/src/js/scripts.js'],
+
+        js:         ['assets/src/js/vendor/modernizr.js',
+                     'assets/src/components/jquery/dist/jquery.js',
+                     'assets/src/components/flexslider/jquery.flexslider.js', // Fancybox JS
+                     'assets/_temp/js/*.js']
+      },
+
+      dest: {
+        sass:    'assets/src/css',
+        css:     'assets/dist', 
+        js:      'assets/dist'
+      },
+
+      temp: {
+        css: 'assets/_temp/css',
+        js: 'assets/_temp/js', 
+      }
+};
 
 
 // SASS
-gulp.task('sass', function() {
-  return sass('assets/src/sass/style.scss', {
-              style: 'expanded',
-              lineNumbers: true,
-              sourcemap: true
+gulp.task('sass', ['clean-css'], function() {
+  return sass(paths.src.sass, {
+      style: 'expanded',
+        lineNumbers: true
     })
     .on('error', sass.logError)
-
-    .pipe(autoprefixer('last 4 version', {map: true}))
-
-    .pipe(sourcemaps.write())
-
-    // For file sourcemaps
-    .pipe(sourcemaps.write('maps', {
-      includeContent: false,
-      sourceRoot: 'source'
-    }))
-
-    // .pipe(cmq({log: true}))
-
-    .pipe(gulp.dest(paths.dest.sass))
-
+    .pipe(autoprefixer('last 4 version', {map: false}))
+   // .pipe(mmq({log: true}))
+    .pipe(gulp.dest(paths.temp.css))
 });
 
 //CSS - Let's combine all css and minify them
 gulp.task('css', ['sass'], function() {
   return gulp.src(paths.src.css)
         .pipe(plumber())
-        .pipe(concat('style.css'))
-        // .pipe(ifElse(isEnv, minifycss))
+        .pipe(concat('styles.css'))
+        .pipe( ifElse(env === 'production', minifycss))
         .pipe(gulp.dest(paths.dest.css))
 });
 
 // Scripts
-gulp.task('scripts', function() {
+gulp.task('scripts', ['clean-js'], function() {
+  return gulp.src(paths.src.custom_js)
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('default'))
+    .pipe(gulp.dest(paths.temp.js))
+});
+
+gulp.task('concact_scripts', ['scripts'], function() {
   return gulp.src(paths.src.js)
     .pipe(plumber())
-    //.pipe(jshint('.jshintrc'))
-    //.pipe(jshint.reporter('default'))
     .pipe(concat('scripts.js'))
-    // .pipe(ifElse(isEnv, uglify))
+    .pipe( ifElse(env === 'production', uglify))
     .pipe(gulp.dest(paths.dest.js))
+});
+
+gulp.task('clean-css', function(){
+  return gulp.src(paths.temp.css)
+        .pipe(clean());
+});
+
+gulp.task('clean-js', function(){
+  return gulp.src(paths.temp.js)
+        .pipe(clean());
 });
 
 
@@ -94,24 +108,21 @@ gulp.task('scripts', function() {
 gulp.task('watch', function() {
 
   // Watch .scss files
-  gulp.watch('assets/src/sass/**/*.scss', ['css', 'sass']);
-
-  // Watch .css files
-  gulp.watch('assets/src/css/*.css', ['css']);
+  gulp.watch('assets/src/sass/**/*.scss', ['sass', 'css']);
 
   // Watch .js files
-  gulp.watch('assets/src/js/**/*.js', ['scripts']);
+  gulp.watch(paths.src.custom_js, ['scripts', 'concact_scripts']);
 
 
 });
 
 
 
-
-
-
 /* TASKS
 ========================================================================== */
 
-// Default task
+// Listing Default Tasks
+var defaultTasks = [ 'sass', 'css', 'scripts', 'concact_scripts', 'watch'];
+
+// Run Default Task task
 gulp.task('default', defaultTasks);
